9aaf98b5925de98776c33b71c3441304f2eb942a
Julian <schubeljulian@gmail.com>
2025-12-26 20:10:22 +0200

    Fix bug: move load_file_buffer to a local function in the window module

--- lua/notepad/window.lua
+++ lua/notepad/window.lua
@@ -1,6 +1,16 @@
-local load_file_buffer = require("files").load_file_buffer;
-
 local Module = {}
+
+local function load_file_buffer(path)
+  -- Check if buffer already exists
+  local buf = vim.fn.bufnr(path, false)
+
+  if buf == -1 then
+    buf = vim.fn.bufadd(path)
+    vim.fn.bufload(buf)
+  end
+
+  return buf
+end
 
 function Module.open(path, opts)
     --If duplicate windows is not enabled in the configuration reuse the
