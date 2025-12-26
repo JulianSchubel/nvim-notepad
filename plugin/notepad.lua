978ec65c8b927e84165c4e1318cedca9bb9c7066
Julian <schubeljulian@gmail.com>
2025-12-27 00:52:14 +0200

    Narrow entry to picker

--- plugin/notepad.lua
+++ plugin/notepad.lua
@@ -1,10 +1,5 @@
-vim.api.nvim_create_user_command("Notepad", function(opts)
-    require("notepad").open(opts.args ~= "" and opts.args or {})
-end, { nargs = "?" })
-
-vim.api.nvim_create_user_command("NotepadPick", function()
+vim.api.nvim_create_user_command("Notepad", function()
     require("notepad.telescope").open(require("notepad.config").opts)
 end, {})
 
 vim.keymap.set("n", "<leader>nn", "<cmd>Notepad<cr>", { desc = "Open Notepad" })
-vim.keymap.set("n", "<leader>np", "<cmd>NotepadPick", { desc = "Open Notepad Picker" })
