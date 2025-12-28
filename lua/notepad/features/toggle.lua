77bd3f8454537d4336a7fb9151d1165b94448bff
Julian <schubeljulian@gmail.com>
2025-12-28 23:46:34 +0200

    Fix bug in toggle feature

--- lua/notepad/features/toggle.lua
+++ lua/notepad/features/toggle.lua
@@ -1,3 +1,4 @@
+local window = require("notepad.window")
 --Toggles a checkbox on the current cursor line between checked / unchecked
 local function toggle()
     --Read the full text of the line under the cursor
@@ -5,12 +6,12 @@ local function toggle()
 
     vim.notify(line, vim.log.levels.WARN)
     --Check for an unchecked task indicator;
-    if line:match("%- %[%]") then
+    if line:match("%- %`%[%s?%]%`") then
         --Replace the unchecked occurence with a checked task indicator
-        line = line:gsub("%- %[x%]", "- [x]", 1)
-    elseif line:match("%- %[x%]") then
+        line = line:gsub("%- %`%[%s?%]%`", "- `[x]`", 1)
+    elseif line:match("%- %`%[x%]%`") then
         --Replace the checked occurence with an unchecked indicator
-        line = line:gsub("%- %[x%]", "- [ ]", 1)
+        line = line:gsub("%- %`%[x%]%`", "- `[ ]`", 1)
     else
         --Do nothing on no match; prevent accidental edits
         return
